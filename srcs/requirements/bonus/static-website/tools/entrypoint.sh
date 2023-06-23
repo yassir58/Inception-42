02d0dc3fee333ec57f7bab2045ba4914c6e200f6
yassir <elatmaniyassir3@gmail.com>
2023-06-23 14:37:39 +0100

    finishing all services, docker compose refactore needed

--- srcs/requirements/bonus/static-website/tools/entrypoint.sh
+++ srcs/requirements/bonus/static-website/tools/entrypoint.sh
@@ -3,7 +3,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.bashrc
 nvm install --lts
 node --version
-
 npm init -y
 npm install express
 exec "$@"
--- srcs/requirements/bonus/static-website/tools/static/index.js
+++ srcs/requirements/bonus/static-website/tools/static/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 const path = require ('path');
+const port = 1234 ;
 
 //setting middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.listen(8080, ()=>{
+app.listen(port, ()=>{
     console.log ("Server listening on port 8080 ...")
 });
